afc1b4cc08da262ae3b1c16819fc6b06461d3139
hemanth kumar <hemanth.sakamuri@imaginate.in>
2020-04-29 17:39:36 +0530

    performance improve

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(compression);
+app.use(compression());
 
 app.use("/payment", require("./routes/payment"));
 
